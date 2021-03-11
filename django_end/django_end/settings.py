90ac8221380ed59e5184824d15c641834113077f
duenchay <duenchay.wo.60@ubu.ac.th>
2021-03-11 23:35:26 +0700

    test

--- django_end/django_end/settings.py
+++ django_end/django_end/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework',
+    # 'rest_framework',
     'django_dbml',
     'django_extensions'
 ]
