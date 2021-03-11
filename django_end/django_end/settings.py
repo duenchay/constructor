9fe51fa9ec9e57538b861ded8dd18bfed403077d
duenchay <duenchay.wo.60@ubu.ac.th>
2021-03-11 23:36:41 +0700

    test

--- django_end/django_end/settings.py
+++ django_end/django_end/settings.py
@@ -53,15 +53,15 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES':  [
-        'rest_framework.permissions.AllowAny']
-    # [
-    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    # ]
-}
+# REST_FRAMEWORK = {
+#     # Use Django's standard `django.contrib.auth` permissions,
+#     # or allow read-only access for unauthenticated users.
+#     'DEFAULT_PERMISSION_CLASSES':  [
+#         'rest_framework.permissions.AllowAny']
+#     # [
+#     #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+#     # ]
+# }
 
 ROOT_URLCONF = 'django_end.urls'
 
